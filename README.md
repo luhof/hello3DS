75ee2672dd5fbe21c954565dd3b2d965e4199d64
Lucas Hörand <lhorand@etud.u-pem.fr>
2016-10-25 16:23:37 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## Hello3DS
+
+Random, quick and dirty 3DS homebrew experiments / boilerplate / thingies.
+Currently exports projects as .3dsx format
+
+## what you need
+
+- 3DS (old or new) with a CFW (homebrew launcher installed)
+
+### dependencies
+
+- devkitPro http://3dbrew.org/wiki/Setting_up_Development_Environment#Linux
+- ctrulib https://github.com/smealum/ctrulib
+- sf2dlib https://github.com/xerpi/sf2dlib
+
+### next
+
+- better hello world-ish (message, user input)
+- image rendering
+- 3D stereoscopic effect with 2D pictures
+- 3D model loading ?
---
+++ source/main.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <string.h>
+#include <3ds.h>
+
+int main()
+{
+  // Initializations
+  srvInit();        // services
+  aptInit();        // applets
+  hidInit();        // input
+  gfxInitDefault(); // graphics
+  gfxSet3D(false);  // 3D effect (false for console display)
+  u32 kDown;        // keys down
+  u32 kHeld;        // keys pressed
+  u32 kUp;          // keys up
+  u8* fbTopLeft;    // top left screen's framebuffer
+  u8* fbTopRight;   // top right screen's framebuffer
+  u8* fbBottom;     // bottom screen's framebuffer
+
+  //displaying a console message on the top screen,
+  consoleInit(GFX_TOP, NULL);
+  printf("\x1b[15;19HHello World!");
+  onsoleInit(GFX_BOTTOM, NULL);
+  printf("\x1b[15;19HPress enter to exit :)");
+
+  // Main loop
+  while (aptMainLoop())
+  {
+
+    // Wait for next frame
+    gspWaitForVBlank();
+
+    // Read which buttons are currently pressed or not
+    hidScanInput();
+    kDown = hidKeysDown();
+    kHeld = hidKeysHeld();
+    kUp = hidKeysUp();
+
+    // If START button is pressed, break loop and quit
+    if (kDown & KEY_START){
+      break;
+    }
+
+    // Reset framebuffers
+    fbTopLeft = gfxGetFramebuffer(GFX_TOP, GFX_LEFT, NULL, NULL);
+    fbTopRight = gfxGetFramebuffer(GFX_TOP, GFX_RIGHT, NULL, NULL);
+    fbBottom = gfxGetFramebuffer(GFX_BOTTOM, 0, NULL, NULL);
+    memset(fbTopLeft, 0, 240 * 400 * 3);
+    memset(fbTopRight, 0, 240 * 400 * 3);
+    memset(fbBottom, 0, 240 * 320 * 3);
+
+
+    /** Your code starts here **/
+
+
+
+    /** End of your code **/
+
+
+    // Flush and swap framebuffers
+    gfxFlushBuffers();
+    gfxSwapBuffers();
+  }
+
+  // Exit
+  gfxExit();
+  hidExit();
+  aptExit();
+  srvExit();
+
+  // Return to hbmenu
+  return 0;
+}
